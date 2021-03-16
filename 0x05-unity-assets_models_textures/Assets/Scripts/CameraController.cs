2f8196772b44cfbe7f1ba6347633cfd389c92541
Giovanni Perez <op7puro@live.com>
2021-03-16 10:25:17 -0500

    Add more task

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotationSpeed;
+    public Transform target, player;
+    float mouseX, mouseY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+
+        target.transform.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        player.transform.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    public float gravity;
+
+    Vector3 velocity;
+
+    CharacterController playerController;
+
+    public bool isGround;
+    public bool isDead = false;
+
+
+    public Transform groundCheck;
+    public float groundDistance = .4f;
+    public LayerMask groundMask;
+    public LayerMask deadMask;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isDead = Physics.CheckSphere(groundCheck.position, groundDistance, deadMask);
+
+        Move();
+
+        if (isDead)
+        {
+            resetPosition();
+        }
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        playerController.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space) && isGround)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -1 * gravity);
+            isGround = false;
+        }
+
+        playerController.Move(velocity * Time.deltaTime);
+
+
+
+    }
+
+    void resetPosition()
+    {
+        transform.position = new Vector3(0, 30f, 0);
+    }
+
+
+}
