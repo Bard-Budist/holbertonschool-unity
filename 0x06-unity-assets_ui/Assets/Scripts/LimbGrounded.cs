31d15b486dde9fed51e31bcd63f70a5b7268aa38
Giovanni Perez <op7puro@live.com>
2021-06-25 16:19:21 -0500

    Add first task

---
+++ 0x06-unity-assets_ui/Assets/Scripts/LimbGrounded.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimbGrounded : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        PlayerController py = other.gameObject.GetComponent<PlayerController>();
+        py.isGround = true;
+        Debug.Log("Buenas");
+    }
+}
